398aceaf34f3f2446120492a03dcbf13f2bf49d2
Alexander Tatchin <alexander.tatchin@gmail.com>
2016-07-29 16:37:46 +0300

    playing with react-router

--- hello-world/src/Layout.js
+++ hello-world/src/Layout.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router';
 import './Layout.css';
 
 class Layout extends Component {
   render() {
     return (
       <div className="Layout">
-        <div className="Layout-header">
-          <img src={logo} className="Layout-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="Layout-intro">
-          To get started, edit <code>src/Layout.js</code> and save to reload.
-        </p>
+        <Link to="home">home</Link>
+        <Link to="about">about</Link>
+        {this.props.children}
       </div>
     );
   }
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
 import Layout from './Layout';
+import Home from './home/Home';
+import About from './about/About';
+
 import './index.css';
 
 ReactDOM.render(
-  <Layout />,
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Home}></IndexRoute>
+      <Route path="home" component={Home}></Route>
+      <Route path="about" component={About}></Route>
+    </Route>  
+  </Router>,
   document.getElementById('root')
 );
+
+
